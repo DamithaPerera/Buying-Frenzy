a68ec19f9ca6e136e183cdbe00a4901f5d94c089
Damitha Perera <damitha.perera@keeneye.solutions>
2022-07-31 22:50:30 +0530

    componetents

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { RestaurantModule } from './restaurant/restaurant.module';
+import { DishModule } from './dish/dish.module';
+import { OpeningHoursModule } from './opening-hours/opening-hours.module';
+import { PurchaseOrderModule } from './purchase-order/purchase-order.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, RestaurantModule, DishModule, OpeningHoursModule, PurchaseOrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
