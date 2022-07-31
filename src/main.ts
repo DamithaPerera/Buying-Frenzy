f2e94dcb534701b47bfff4fe4bcf8ac6874d868c
Damitha Perera <damitha.perera@keeneye.solutions>
2022-07-31 22:35:58 +0530

    middleware set

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Buying Frenzy')
+    .setDescription('Buying Frenzy API Collection')
+    .setVersion('1.0')
+    .addTag('buying_frenzy')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3001);
 }
 bootstrap();
